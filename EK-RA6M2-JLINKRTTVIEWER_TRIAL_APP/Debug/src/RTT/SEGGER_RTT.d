9dd3260143d9a77832351e1c5d423379e05f678c
ferdikurnazdm <ferdikurnazdm@gmail.com>
2023-03-14 14:36:30 +0300

    JLINKRTT_TRIAL

---
+++ EK-RA6M2-JLINKRTTVIEWER_TRIAL_APP/Debug/src/RTT/SEGGER_RTT.d
@@ -0,0 +1,4 @@
+src/RTT/SEGGER_RTT.o: ../src/RTT/SEGGER_RTT.c ../src/RTT/SEGGER_RTT.h \
+ C:/Users/ferdi.kurnaz/e2_studio/workspace/Blink_deneme_4/src/RTT/SEGGER_RTT_Conf.h
+../src/RTT/SEGGER_RTT.h:
+C:/Users/ferdi.kurnaz/e2_studio/workspace/Blink_deneme_4/src/RTT/SEGGER_RTT_Conf.h:
---
+++ EK-RA6M2-JLINKRTTVIEWER_TRIAL_APP/Debug/src/RTT/SEGGER_RTT_printf.d
@@ -0,0 +1,7 @@
+src/RTT/SEGGER_RTT_printf.o: ../src/RTT/SEGGER_RTT_printf.c \
+ ../src/RTT/SEGGER_RTT.h \
+ C:/Users/ferdi.kurnaz/e2_studio/workspace/Blink_deneme_4/src/RTT/SEGGER_RTT_Conf.h \
+ ../src/RTT/SEGGER_RTT_Conf.h
+../src/RTT/SEGGER_RTT.h:
+C:/Users/ferdi.kurnaz/e2_studio/workspace/Blink_deneme_4/src/RTT/SEGGER_RTT_Conf.h:
+../src/RTT/SEGGER_RTT_Conf.h:
